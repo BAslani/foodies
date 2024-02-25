7b45689a4c90ee3a17576ffeb235c679fef7cecb
Behzad Aslani <behzadaslani80@gmail.com>
2024-02-25 13:39:24 +0330

    fix: cleaned out the header's code

--- components/MainHeader/MainHeader.tsx
+++ components/MainHeader/MainHeader.tsx
@@ -2,25 +2,30 @@ import Link from "next/link"
 import React from "react"
 import logo from "@/assets/logo.png"
 import styles from "./mainHeader.module.css"
+import Image from "next/image"
+import Background from "./Background"
 
 const MainHeader = () => {
   return (
-    <header className={styles.header}>
-      <Link className={styles.logo} href={"/"}>
-        <img src={logo.src} alt='a plate with food' />
-        Next Level Food
-      </Link>
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <Link href={"/meals"}>Browse Meals</Link>
-          </li>
-          <li>
-            <Link href={"/community"}>Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <Background />
+      <header className={styles.header}>
+        <Link className={styles.logo} href={"/"}>
+          <Image src={logo} alt='a plate with food' priority />
+          Next Level Food
+        </Link>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href={"/meals"}>Browse Meals</Link>
+            </li>
+            <li>
+              <Link href={"/community"}>Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   )
 }
 
