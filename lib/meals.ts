73dc26ff33cab3dd5bc461763ec800ef819630dc
Behzad Aslani <behzadaslani80@gmail.com>
2024-03-09 00:20:30 +0330

    feat: implemented detailed meal dynamic page

--- lib/meals.ts
+++ lib/meals.ts
@@ -6,3 +6,7 @@ const db = sql("meals.db")
 export const getMeals = (): MealTypes[] => {
   return db.prepare("SELECT * FROM meals").all() as MealTypes[]
 }
+
+export const getMeal = (slug: string): MealTypes => {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug) as MealTypes
+}
